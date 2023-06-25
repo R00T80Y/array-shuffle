f2ed1ae88de2d46311d35ec1e6f92fd139e310ed
nnu17ip1y <nnu17ip1y@yandex.ru>
2023-06-25 23:42:27 +0300

    fix: quote

--- src/shuffle.test.ts
+++ src/shuffle.test.ts
@@ -1,9 +1,9 @@
-import { describe, expect, it } from "vitest";
+import { describe, expect, it } from 'vitest';
 
-import { shuffle } from "./index";
+import { shuffle } from './index';
 
-describe("Whatever", () => {
-  it("Should pass CI", () => {
+describe('Whatever', () => {
+  it('Should pass CI', () => {
     expect(shuffle([1])).toEqual([1]);
 
     expect(shuffle([3, 2, 1])).toContain(1);
